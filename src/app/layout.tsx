0a53f87edfdd5249724e58736bbaf6838b71ec0a
Ashok Jaiswal <ashok.jaiswal@gmail.com>
2026-01-24 13:10:26 +0800

    Could not fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,8 +28,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className="antialiased">
+      <body className="antialiased">{children}</body>
     </html>
   );
 }
